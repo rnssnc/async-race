b5d7c22cb37254513d298cc9db3ee091684b184c
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-06-13 22:26:58 +0300

    feat: add error & loading handling components

---
+++ src/components/ErroIndicator/ErrorIndicatort.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './ErrorIndicator.sass';
+
+const ErrorIndicator = () => {
+  return (
+    <div className="error-indicator">
+      <span className="h1">
+        You found an error :( Please contact me on discord - Renaissance#6666
+      </span>
+    </div>
+  );
+};
+
+export default ErrorIndicator;
