f1bfc01afbe45ec85aa9912c865e91272589557f
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-06-13 22:30:34 +0300

    feat: add garage & winners pages

---
+++ src/components/pages/Garage.tsx
@@ -0,0 +1,343 @@
+import React from 'react';
+
+import CarService, { TCar, CarStatus, IEngineData, RaceStatus } from '../../services/CarService';
+import ErrorBoundry from '../ErrorBoundry/ErrorBoundry';
+import CarForm from '../CarForm/CarForm';
+import CarUpdateForm from '../CarUpdateForm/CarUpdateForm';
+import CarsActions from '../CarsActions/CarsActions';
+import CarsGarage from '../CarsGarage/CarsGarage';
+import WinnerModalWindow from '../WinnerModalWindow/WinnerModalWindow';
+
+const CREATE_CAR_DEFAULT_STATE = {
+  value: '',
+  color: '#ffffff',
+};
+
+const CAR_BRANDS_PATH = 'json/carBrands.json';
+const CAR_MODELS_PATH = 'json/carModels.json';
+
+type State = {
+  currentCar: TCar | null;
+  winnerCar: {
+    name: string;
+    time: number;
+  } | null;
+  isWinnerPopupShown: boolean;
+  totalCarsCount: number;
+  cars: TCar[];
+  page: number;
+  totalPagesCount: number;
+  raceStatus: RaceStatus;
+  isLoading: boolean;
+};
+
+interface IProps {
+  page: number;
+  onUnmount: (page: number) => void;
+}
+
+export default class GaragePage extends React.Component<IProps, {}> {
+  readonly CARS_TO_GENERATE = 100;
+  readonly CARS_PER_PAGE = 7;
+
+  state: State = {
+    cars: [],
+    currentCar: null,
+    winnerCar: null,
+    totalCarsCount: 0,
+    page: 1,
+    totalPagesCount: 0,
+    raceStatus: RaceStatus.ready,
+    isWinnerPopupShown: false,
+    isLoading: false,
+  };
+
+  componentDidMount() {
+    this.getCars(this.props.page);
+  }
+
+  componentWillUnmount() {
+    this.props.onUnmount(this.state.page);
+  }
+
+  render() {
+    const {
+      isLoading,
+      currentCar,
+      winnerCar,
+      cars,
+      totalCarsCount,
+      page,
+      totalPagesCount,
+      raceStatus,
+      isWinnerPopupShown,
+    } = this.state;
+
+    return (
+      <ErrorBoundry>
+        <section className="section">
+          <div className="car-forms-wrapper d-flex flex-wrap pt-4">
+            <CarForm defaultState={CREATE_CAR_DEFAULT_STATE} onItemSubmit={this.addCar} />
+            <CarUpdateForm
+              currentCar={currentCar}
+              defaultState={CREATE_CAR_DEFAULT_STATE}
+              value={currentCar?.name || ''}
+              color={currentCar?.color || ''}
+              onItemSubmit={this.updateCar}
+            />
+          </div>
+          <CarsActions
+            raceStatus={raceStatus}
+            onRaceClick={this.carsRace}
+            onGenerateClick={() => this.generateCars(this.CARS_TO_GENERATE)}
+            onResetClick={this.carsReset}
+            isLoading={isLoading}
+          />
+        </section>
+        <CarsGarage
+          cars={cars}
+          carsCount={totalCarsCount}
+          raceStatus={raceStatus}
+          page={page}
+          pageCount={totalPagesCount}
+          onNextPage={() => this.getCars(this.state.page + 1)}
+          onPrevPage={() => this.getCars(this.state.page - 1)}
+          onSelect={this.onSelect}
+          onRemove={this.onRemove}
+          onEngineStart={this.onEngineStart}
+          onEngineStop={this.onEngineStop}
+        />
+        <WinnerModalWindow
+          isShown={isWinnerPopupShown}
+          carWinner={winnerCar}
+          onClose={this.onWinnerModalClose}
+        ></WinnerModalWindow>
+      </ErrorBoundry>
+    );
+  }
+  onWinnerModalClose = (): void => {
+    this.setState({
+      isWinnerPopupShown: false,
+      winnerCar: null,
+    });
+  };
+
+  onNextPage = (): void => {
+    this.getCars(this.state.page + 1);
+  };
+
+  onPrevPage = (): void => {
+    this.getCars(this.state.page - 1);
+  };
+
+  onEngineStop = (id: any): void => {
+    CarService.stopEngine(id).then((engineData) => {
+      this.setState(({ cars }: State) => {
+        const newCarData = cars.map((car) => {
+          if (car.id === id) {
+            car.status = undefined;
+            car.driveDuration = 0;
+          }
+          return car;
+        });
+
+        return {
+          cars: newCarData,
+        };
+      });
+    });
+  };
+
+  carsRace = () => {
+    this.setState({ isLoading: true, raceStatus: RaceStatus.race });
+
+    const enginePromises = this.state.cars.map((car) => CarService.startEngine(car.id));
+
+    Promise.all(enginePromises).then((engineDataResponses) => {
+      this.startCars(engineDataResponses);
+
+      this.setState({ isLoading: false });
+
+      const drivePromises = this.state.cars.map((car) => {
+        return new Promise<{ id: number; success: boolean }>((resolve) =>
+          CarService.drive(car.id).then((data) => {
+            if (!data.success) this.stopCar(car.id);
+
+            resolve(data);
+          }),
+        );
+      });
+
+      Promise.all(drivePromises).then((data) => {
+        const winner = this.getWinner(data);
+
+        if (winner) {
+          CarService.saveWinner(winner.id, winner.driveDuration).then((winner) => {
+            const name = this.state.cars.find((car) => car.id === winner.id)?.name;
+
+            this.setState({
+              isWinnerPopupShown: true,
+              raceStatus: RaceStatus.complete,
+              winnerCar: { name, time: winner.time },
+            });
+          });
+        } else {
+          this.setState({
+            raceStatus: RaceStatus.complete,
+            isWinnerPopupShown: true,
+          });
+        }
+      });
+    });
+  };
+
+  onEngineStart = (id: any): void => {
+    CarService.startEngine(id).then((engineData) => {
+      this.startCar(id, engineData);
+
+      CarService.drive(id).then((data) => {
+        if (!data.success) this.stopCar(id);
+      });
+    });
+  };
+
+  private getWinner(data: { id: number; success: boolean }[]) {
+    const finishedCars = this.state.cars.filter((car, index) => (data[index].success ? car : null));
+
+    if (finishedCars.length === 0) return false;
+
+    return finishedCars.reduce((prev, curr) =>
+      prev.driveDuration < curr.driveDuration ? prev : curr,
+    );
+  }
+
+  private startCar(id: number, engineData: IEngineData) {
+    this.setState(({ cars }: State) => {
+      const newCarData = cars.map((car) => {
+        if (car.id === id) {
+          car.status = CarStatus.drive;
+          car.driveDuration = Math.round(engineData.distance / engineData.velocity) / 1000;
+        }
+        return car;
+      });
+
+      return {
+        cars: newCarData,
+      };
+    });
+  }
+
+  private startCars(engineData: IEngineData[]) {
+    this.setState(({ cars }: State) => {
+      const newCarData = cars.map((car, index) => {
+        car.status = CarStatus.drive;
+        car.driveDuration =
+          Math.round(engineData[index].distance / engineData[index].velocity) / 1000;
+
+        return car;
+      });
+
+      return {
+        cars: newCarData,
+      };
+    });
+  }
+
+  carsReset = () => {
+    this.setState({ isLoading: true });
+
+    const enginePromises = this.state.cars.map((car) => CarService.stopEngine(car.id));
+
+    Promise.all(enginePromises).then(() => {
+      this.setState(({ cars }: State) => {
+        const newCarData = cars.map((car) => {
+          car.status = undefined;
+          car.driveDuration = 0;
+
+          return car;
+        });
+
+        return {
+          raceStatus: RaceStatus.ready,
+          isLoading: false,
+          cars: newCarData,
+        };
+      });
+    });
+  };
+
+  private stopCar(id: number) {
+    this.setState(({ cars }: State) => {
+      const newCarData = cars.map((car) =>
+        car.id === id ? ((car.status = CarStatus.stopped), car) : car,
+      );
+
+      return {
+        cars: newCarData,
+      };
+    });
+  }
+
+  onRemove = (id: any): void => {
+    Promise.all([CarService.deleteCar(id), CarService.deleteWinner(id)]).then(() => this.getCars());
+  };
+
+  onSelect = (car: TCar): void => {
+    this.setState({
+      currentCar: car,
+    });
+  };
+
+  addCar = (name: string, color: string, doUpdate = true) => {
+    CarService.createCar({ name, color }).then(() => {
+      if (doUpdate) this.getCars();
+    });
+  };
+
+  updateCar = (name: string, color: string) => {
+    const carName = name ? name : 'unnamed';
+
+    if (this.state.currentCar?.id) {
+      CarService.updateCar(this.state.currentCar.id, {
+        name: carName,
+        color,
+      }).then(() => {
+        this.getCars();
+        this.setState({
+          currentCar: null,
+        });
+      });
+    }
+  };
+
+  private generateRandomColor(): string {
+    return '#' + (Math.random().toString(16) + '00000').slice(2, 8);
+  }
+
+  generateCars = (carsToGeterate: number) => {
+    Promise.all([fetch(CAR_BRANDS_PATH), fetch(CAR_MODELS_PATH)]).then((responses) => {
+      const resp = responses.map((response) => response.json());
+      Promise.all(resp).then(([brands, models]) => {
+        for (let i = 0; i < carsToGeterate; i++) {
+          const randomBrand = brands[Math.floor(Math.random() * brands.length)];
+          const randomModel = models[Math.floor(Math.random() * models.length)];
+          const randomColor = this.generateRandomColor();
+
+          const doUpdate = i === this.CARS_TO_GENERATE - 1;
+          this.addCar(`${randomBrand} ${randomModel}`, randomColor, doUpdate);
+        }
+      });
+    });
+  };
+
+  getCars = (page = this.state.page, limit = this.CARS_PER_PAGE) => {
+    CarService.getCars(page, limit).then(({ cars, count }) => {
+      this.setState({
+        cars,
+        totalCarsCount: count,
+        page,
+        totalPagesCount: Math.ceil(count / limit),
+      });
+    });
+  };
+}
---
+++ src/components/pages/Winners.tsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import ErrorBoundry from '../ErrorBoundry/ErrorBoundry';
+import CarService, {
+  TWinner,
+  CarsWinnersOrderBy,
+  CarsWinnersSortBy,
+} from '../../services/CarService';
+import WinnersTable from '../WinnersTable/WinnersTable';
+import Pagination from '../Pagination/Pagination';
+
+type TState = {
+  winners: TWinner[] | null;
+  totalPagesCount: number;
+  totalWinnersCount: number;
+  page: number;
+  sortOrder: CarsWinnersOrderBy;
+  sortBy: CarsWinnersSortBy;
+};
+
+export default class WinnersPage extends React.Component {
+  readonly WINNERS_PER_PAGE = 10;
+
+  state: TState = {
+    winners: null,
+    totalPagesCount: 0,
+    totalWinnersCount: 0,
+    page: 1,
+    sortOrder: CarsWinnersOrderBy.ASC,
+    sortBy: CarsWinnersSortBy.wins,
+  };
+  componentDidUpdate = (_prevProps: {}, _prevState: TState) => {
+    if (_prevState.sortBy !== this.state.sortBy || _prevState.sortOrder !== this.state.sortOrder)
+      this.getWinners();
+  };
+
+  componentDidMount = () => {
+    this.getWinners();
+  };
+
+  render() {
+    const { page, totalWinnersCount, totalPagesCount, winners, sortOrder, sortBy } = this.state;
+    console.log(this.state);
+    const content =
+      winners === null ? (
+        'No winners found :( Try to make a race first!'
+      ) : (
+        <WinnersTable
+          onTimeClick={this.onTimeFieldClick}
+          onWinsClick={this.onWinsFieldClick}
+          sortOrder={sortOrder}
+          sortBy={sortBy}
+          winners={winners}
+        />
+      );
+
+    return (
+      <ErrorBoundry>
+        <section className="section winners-section pt-4">
+          <div className="winners-section__heading-pagination-wrapper d-flex align-items-center">
+            <h2 className="mb-0">Winners{totalWinnersCount > 0 ? `(${totalWinnersCount})` : ''}</h2>
+            <Pagination
+              isLocked={false}
+              page={page}
+              pageCount={totalPagesCount}
+              onNextPage={() => this.getWinners(this.state.page + 1)}
+              onPrevPage={() => this.getWinners(this.state.page - 1)}
+            />
+          </div>
+          {content}
+        </section>
+      </ErrorBoundry>
+    );
+  }
+
+  onTimeFieldClick = () => {
+    this.setState(({ sortBy, sortOrder }: TState) => {
+      if (sortBy === CarsWinnersSortBy.time) {
+        console.log('true');
+        return { sortOrder: this.switchSortOrder(sortOrder) };
+      } else return { sortBy: CarsWinnersSortBy.time };
+    });
+  };
+
+  private switchSortOrder(sortOrder: CarsWinnersOrderBy) {
+    return sortOrder === CarsWinnersOrderBy.ASC ? CarsWinnersOrderBy.DESC : CarsWinnersOrderBy.ASC;
+  }
+
+  onWinsFieldClick = () => {
+    this.setState(({ sortBy, sortOrder }: TState) => {
+      if (sortBy === CarsWinnersSortBy.wins) {
+        return { sortOrder: this.switchSortOrder(sortOrder) };
+      } else return { sortBy: CarsWinnersSortBy.wins };
+    });
+  };
+
+  getWinners = (page = this.state.page, limit = this.WINNERS_PER_PAGE) => {
+    const { sortOrder, sortBy } = this.state;
+
+    CarService.getWinners({
+      page,
+      limit,
+      sort: sortBy,
+      order: sortOrder,
+    }).then((data) => {
+      const winners: TWinner[] = [];
+
+      const getCarPromises = data.items.map((item) =>
+        CarService.getCar(item.id).then((car) =>
+          winners.push({
+            id: item.id,
+            color: car.color,
+            name: car.name,
+            time: item.time,
+            wins: item.wins,
+          }),
+        ),
+      );
+
+      Promise.all(getCarPromises).then(() => {
+        this.setState({
+          winners,
+          totalWinnersCount: data.count,
+          totalPagesCount: Math.ceil(data.count / limit),
+          page,
+        });
+      });
+    });
+  };
+}
